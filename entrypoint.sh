197d8003d1011e08732b500426b82917af38bfae
Clayton van der Haar <haar36@gmail.com>
2022-11-22 07:57:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "[default]
 aws_access_key_id = ${aws_access_key_id_tooling}
-aws_secret_access_key = ${aws_secret_access_key_tooling}" > ~/.aws/credentials
+aws_secret_access_key = ${aws_secret_access_key_tooling}"
 
 aws s3 cp ${file} s3://${aws_s3_bucket}${aws_s3_path} \
             ${aws_region} $*
